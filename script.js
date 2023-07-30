1b61258bdbb2cbe3aa49f3a304fc0bbeddc5622f
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-30 20:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-let team =players;
-let team1 = ...players;
-let cap1 =...person;
+let team =window.players;
+let team1 = [];
+for (let i = 0; i < players.length; i++) {
+	team1[i].push(players.pop());
+}
+let cap1 ={
+	name:person.name,
+	age:person.age,
+};
 window.players = players;
 window.person = person;
 window.team = team;
